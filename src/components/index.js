3d44d6ef249ff4b4dab33a10141d2a720eb67d40
haneef <haneef.hakeem@sedaniainnovator.com>
2023-11-27 15:15:06 +0800

    Initial setup

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Navbar from './Navbar'
+import Hero from './Hero'
+import Intro from './Intro'
+import Favorite from './Favorite'
+import Work from './Work'
+import Service from './Service'
+import Testimonials from './Testimonials'
+import CTA from './CTA'
+import Footer from './Footer'
+
+export {
+    Navbar,
+    Hero,
+    Intro,
+    Favorite,
+    Work,
+    Service,
+    Testimonials,
+    CTA,
+    Footer
+}
